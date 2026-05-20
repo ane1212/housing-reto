7717bf185d7c7167121b952b07070639b3adb5e2
frank <frankrochacahuasiri@gmail.com>
2026-05-20 12:51:53 +0200

    formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-
 import './App.css'
-
+import Formulario from './components/form'
 function App() {
 
   return (
     <>
       <h1>Housing</h1>
+      <div>
+        <Formulario />
+      </div>
     </>
   )
 }
